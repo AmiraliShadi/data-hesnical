73f7ae8423e46d58393aae86d04abbb48cf48c2c
EngRobot33 <hmdkhsrvee@gmail.com>
2024-08-08 10:40:09 +0330

    feat: Implement upload report API

--- dataparser/urls.py
+++ dataparser/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('html-parser/', views.HTMLParserApi.as_view(), name='html-parser'),
+    path('upload-report/', views.ReportUploadApi.as_view(), name='upload-report'),
 ]
--- dataparser/views.py
+++ dataparser/views.py
@@ -1,7 +1,7 @@
 import os
 
-from bs4 import BeautifulSoup
 from django.conf import settings
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.views import APIView
 
 from api import status
@@ -10,7 +10,7 @@ from dataparser.utils import extract_data_from_html_file
 
 
 class HTMLParserApi(APIView):
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         account_id = request.data.get('account_id')
         if not account_id:
             return custom_response(status_code=status.BAD_REQUEST_400)
@@ -28,3 +28,35 @@ class HTMLParserApi(APIView):
         data = extract_data_from_html_file(report_file)
 
         return custom_response(data=data, status_code=status.OK_200)
+
+
+class ReportUploadApi(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request):
+        if 'file' not in request.FILES:
+            return custom_response(error='No file provided.', status_code=status.BAD_REQUEST_400)
+
+        file = request.FILES['file']
+
+        filename = file.name
+        if not filename.startswith('ReportHistory-') or not filename.endswith('.html'):
+            return custom_response(error='File name format is invalid.', status_code=status.BAD_REQUEST_400)
+
+        core_name = filename[len('ReportHistory-'):-len('.html')]
+        if not core_name.isdigit():
+            return custom_response(error='Account ID must be numeric.', status_code=status.BAD_REQUEST_400)
+
+        account_id = core_name
+
+        directory = os.path.join(settings.BASE_DIR, f"HTMLs/{account_id}")
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        save_path = os.path.join(directory, f'{account_id}_report.html')
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        return custom_response(status_code=status.CREATED_201)
